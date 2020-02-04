317e64618353a841942978c3ad8a65ed3ac0066c
apozel <52205094+apozel@users.noreply.github.com>
2020-02-04 15:03:07 +0100

    multi dex qui marche

--- app/src/main/java/fr/isen/walkbook/WallActivity.kt
+++ app/src/main/java/fr/isen/walkbook/WallActivity.kt
@@ -2,11 +2,36 @@ package fr.isen.walkbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+import com.google.firebase.firestore.FirebaseFirestore
 
 class WallActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wall)
+
     }
+
+    fun writeData() {
+        val database = FirebaseFirestore.getInstance()
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        database.collection("user")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
+    }
+
 }
