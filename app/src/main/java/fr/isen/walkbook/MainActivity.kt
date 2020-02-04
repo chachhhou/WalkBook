3193001fbecc154bfd0b85fb09aefce0abd21ad1
apozel <52205094+apozel@users.noreply.github.com>
2020-02-04 11:25:02 +0100

    mainActivity avec ses boutons. il suffit de rajouter le code pour les activités dans les listeners.

--- app/src/main/java/fr/isen/walkbook/MainActivity.kt
+++ app/src/main/java/fr/isen/walkbook/MainActivity.kt
@@ -1,12 +1,33 @@
 package fr.isen.walkbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logOutButton.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
+
+        addButton.setOnClickListener {
+
+        }
+
+        profilButton.setOnClickListener {
+
+        }
+
+        wallButton.setOnClickListener {
+            
+        }
     }
 }
