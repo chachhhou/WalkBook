78f2550dec7e9c5e3b9c10608115bcc2b525f06d
apozel <52205094+apozel@users.noreply.github.com>
2020-02-10 14:26:04 +0100

    postModel

---
+++ app/src/main/java/fr/isen/walkbook/models/PostModel.kt
@@ -0,0 +1,9 @@
+package fr.isen.walkbook.models
+
+data class PostModel (
+
+    val PathPicture: String? = null,
+    val date: String? = null,
+    val description: String? = null,
+    val userId: String? = null
+)
